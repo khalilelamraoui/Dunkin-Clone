4e1245be2a74c6f84a6bb355d14a281137587c9e
El Amraoui Khalil <khalilelam16@gmail.com>
2023-11-24 22:07:57 +0100

    added signinup pages with their styles and scripts

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,42 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//     document.getElementById("months").classList.toggle("show");
+//     document.getElementById("days").classList.toggle("show");
+//   }
+// window.onclick = function(e) {
+//   if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+//   if (!e.target.matches('.dropbtn2')){
+//     var myDropdown2 = document.getElementById("months");
+//     if (myDropdown2.classList.contains('show')) {
+//       myDropdown2.classList.remove('show');
+//     }
+//   }
+//   if (!e.target.matches('.dropbtn3')){
+//     var myDropdown3 = document.getElementById("days");
+//     if (myDropdown3.classList.contains('show')) {
+//       myDropdown3.classList.remove('show');
+//     }
+//   }
+// }
+
+function myFunction(dropdownId) {
+  var dropdown = document.getElementById(dropdownId);
+  dropdown.classList.toggle("show");
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
       }
-    }
   }
+}
